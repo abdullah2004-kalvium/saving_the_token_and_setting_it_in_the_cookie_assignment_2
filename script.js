1d3b6f95ccebff1f3999b3124c8a362e469486b4
abdullah2004-kalvium <abdullahm6270@gmail.com>
2025-04-17 09:47:36 +0530

    2nd.done

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-const encrypt = (payload, secret) => {
-  // your code here and return token
+const jwt = require("jsonwebtoken");
+const crypto = require("crypto");
+
+const JWT_SECRET = "vignesh123";
+const ENCRYPTION_KEY = crypto.randomBytes(32);
+const IV = crypto.randomBytes(16);
+
+const encrypt = (payload) => {
+  const token = jwt.sign(payload, JWT_SECRET, { expiresIn: "1h" });
+
+  const cipher = crypto.createCipheriv("aes-256-cbc", ENCRYPTION_KEY, IV);
+  let encrypted = cipher.update(token, "utf8", "hex");
+  encrypted += cipher.final("hex");
+
+  return {
+    encryptedToken: encrypted,
+    iv: IV.toString("hex"),
+  };
 };
 
-module.exports = encrypt;
+const decrypt = ({ encryptedToken, iv }) => {
+  const decipher = crypto.createDecipheriv(
+    "aes-256-cbc",
+    ENCRYPTION_KEY,
+    Buffer.from(iv, "hex")
+  );
+
+  let decrypted = decipher.update(encryptedToken, "hex", "utf8");
+  decrypted += decipher.final("utf8");
+
+  const decoded = jwt.verify(decrypted, JWT_SECRET);
+  return decoded;
+};
+
+module.exports = {
+  encrypt,
+  decrypt,
+};
